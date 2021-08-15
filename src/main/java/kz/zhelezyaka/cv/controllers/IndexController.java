7c391acb41434eb948586adc065f04712272195a
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-16 01:25:20 +0600

    Added logger

--- src/main/java/kz/zhelezyaka/cv/controllers/IndexController.java
+++ src/main/java/kz/zhelezyaka/cv/controllers/IndexController.java
@@ -1,13 +1,17 @@
 package kz.zhelezyaka.cv.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IndexController.class);
 
     @RequestMapping("/")
-    public String index(){
+    public String index() {
+        LOGGER.info("index page");
         return "index";
     }
 }
