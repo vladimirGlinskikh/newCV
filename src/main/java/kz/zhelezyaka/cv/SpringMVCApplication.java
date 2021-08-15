beafed454d048d88df00c6f3c99f41d8d0ddb316
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2021-08-16 00:45:04 +0600

    Create index test page and controller

---
+++ src/main/java/kz/zhelezyaka/cv/SpringMVCApplication.java
@@ -0,0 +1,11 @@
+package kz.zhelezyaka.cv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMVCApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMVCApplication.class, args);
+    }
+}
---
+++ src/main/java/kz/zhelezyaka/cv/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.cv.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+}
